1cde92421a5547bab5690ef33384c68bb95d9166
Mateusz Nowak <novak.mateusz94@gmail.com>
2021-04-02 21:46:19 +0200

    Added image example

---
+++ image_app.py
@@ -0,0 +1,23 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang.builder import Builder
+
+Builder.load_file('design_files/image.kv')
+
+
+class MyImage(Widget):
+    def clear_text(self):
+        self.ids.name.text = ''
+        self.ids.surname.text = ''
+        self.ids.age.text = ''
+
+
+class MyApp(App):
+    def build(self):
+        return MyImage()
+
+
+if __name__ == '__main__':
+    app = MyApp()
+    app.run()
